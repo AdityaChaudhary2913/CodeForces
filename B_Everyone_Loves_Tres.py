5245115e90691b777d3f09bccc17b1d1b5d40f2b
AdityaTheJaat <adityachaudhary1306@gmail.com>
2025-01-28 00:03:15 +0530

    Add solution for Codeforces problem B - Everyone Loves Tres

---
+++ B_Everyone_Loves_Tres.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n == 1 or n == 3:
+        print("-1")
+    elif n % 2 == 0:
+        for i in range(n-2):
+            print("3", end="")
+        print("66")
+    else:
+        for i in range(n-5):
+            print("3", end="")
+        print("36366")
